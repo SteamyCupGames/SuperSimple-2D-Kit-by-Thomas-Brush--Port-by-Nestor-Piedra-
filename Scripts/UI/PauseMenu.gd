1cc3b4a0a530b74fbff1b4075402e83ab1ff2cf1
SteamyCup Games <115302416+SteamyCupGames@users.noreply.github.com>
2024-05-03 18:44:36 -0600

    Fixing bugs

--- Scripts/UI/PauseMenu.gd
+++ Scripts/UI/PauseMenu.gd
@@ -1,14 +1,16 @@
 extends CanvasLayer
 
 @onready var main = $"../"
-		
+
 func _on_resume_pressed():
 	main.pauseMenu()
 
 func _on_restart_pressed():
 	SceneTransition.change_scene("res://Scenes/Level1.tscn")
 	Engine.time_scale = 1
+	self.visible = false
 
 func _on_quit_pressed():
 	SceneTransition.change_scene("res://Scenes/MainMenu.tscn")
 	Engine.time_scale = 1
+	self.visible = false
